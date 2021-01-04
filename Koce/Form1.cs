a8481e8e3bcddc4647973436307eb8b070ebf372
Tomaz Cede <tomaz.cede@scv.si>
2021-01-04 13:09:39 +0100

    Add project files.

---
+++ Koce/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace Koce
+{
+    public partial class Form1 : Form
+    {
+        string ime = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection("Server=rogue.db.elephantsql.com; User Id=clhpojwc;" + "Password=wm7N_asXtodPaLSASbaFBEAcB1MtcKMU; Database=clhpojwc;"))
+            {
+                con.Open();
+                NpgsqlCommand com = new NpgsqlCommand("SELECT * FROM kraji", con);
+                NpgsqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    ime = reader.GetString(1);
+                }
+                con.Close();
+            }
+
+            label1.Text = ime;
+        }
+    }
+}

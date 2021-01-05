be77f21b859cc0359b767d1a6b33c2eb25ea26c4
Tomaz Cede <tomaz.cede@scv.si>
2021-01-05 20:36:21 +0100

    Ced je luskan

---
+++ Koce/Form3.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Koce
+{
+    partial class kocapage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(66, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Opis:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(71, 296);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(671, 246);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Lastnosti:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(74, 147);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(668, 84);
+            this.listBox1.TabIndex = 6;
+            // 
+            // kocapage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 567);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "kocapage";
+            this.Text = "Koca";
+            this.Load += new System.EventHandler(this.kocapage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
--- Koce/Form3.cs
+++ Koce/Form3.cs
@@ -11,36 +11,35 @@ using System.Windows.Forms;
 
 namespace Koce
 {
-    public partial class koce_form : Form
+    public partial class kocapage : Form
     {
-        public koce_form()
+        string ime_k = "";
+        public kocapage(string koceime)
         {
             InitializeComponent();
+
+            ime_k = koceime;
         }
 
-        List<string> ime_koce = new List<string>();
-
-        private void kocelist_SelectedIndexChanged(object sender, EventArgs e)
+        private void kocapage_Load(object sender, EventArgs e)
         {
-            string a = kocelist.SelectedItem.ToString();
+            label1.Text = ime_k;
 
-
-            kocapage koca = new kocapage(a);
-            koca.Show();
-        }
-
-        private void Form2_Load(object sender, EventArgs e)
-        {
             using (NpgsqlConnection con = new NpgsqlConnection("Server=rogue.db.elephantsql.com; User Id=clhpojwc;" + "Password=wm7N_asXtodPaLSASbaFBEAcB1MtcKMU; Database=clhpojwc;"))
             {
                 con.Open();
-                NpgsqlCommand com = new NpgsqlCommand("SELECT ime_koce FROM vsekoce()", con);
+                NpgsqlCommand com = new NpgsqlCommand("SELECT * FROM vrnikraj('"+ ime_k +"')", con);
                 NpgsqlDataReader reader = com.ExecuteReader();
                 while (reader.Read())
                 {
-                    kocelist.Items.Add(reader.GetString(0));
+                    label1.Text = reader.GetString(0);
+                    richTextBox1.Text = reader.GetString(1);
 
-                    ime_koce.Add(reader.GetString(0));
+                    string nadmorska = "Nadmorska visina: " + reader.GetInt32(2) + "m";
+                    string gora = "Gora: " + reader.GetString(3);
+                    listBox1.Items.Add(nadmorska);
+                    listBox1.Items.Add(gora);
+
                 }
                 con.Close();
             }
